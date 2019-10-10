4af25d6ed2dcd6a70b3b4402470d84ff634cf63b
adicohen253 <55681305+adicohen253@users.noreply.github.com>
2019-10-10 20:36:34 +0300

    latest

--- zip uploader.py
+++ zip uploader.py
@@ -1,6 +1,14 @@
 import socket
 
-HTTP_RESPONSE = ""
+
+FILE = "installer.zip"
+HTTP_RESPONSE = b"""HTTP/1.1 200 OK
+Content-Type: zip; charset=utf-8
+Content-Disposition: attachment; filename="installer.zip
+Connection: keep-alive
+
+"""
+                
 
 def my_ip():
     """return my current ip in string"""
@@ -12,12 +20,22 @@ def my_ip():
     return s.getsockname()[0]
 
 
+def is_installer_req(request):
+    return request.startswith(b"GET") and b"/installer.zip" in request and b"HTTP/1.1\r\n" in request
+
+
 def main():
+    print(f"my ip is: {my_ip()}")
     server = socket.socket()
     server.bind((my_ip(), 50000))
     server.listen(1)
     client, address = server.accept()
-    x = 4
+    request = client.recv(1024)
+    if is_installer_req(request):
+        with open(FILE, 'rb') as my_file:
+            data = my_file.read()
+            client.send(HTTP_RESPONSE + data)
+    client.close()
 
 
 if __name__ == '__main__':
