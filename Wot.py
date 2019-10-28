36b276c27a8a1919f5f89b856832ebabfba34bd4
adi <adic2531@gmail.com>
2019-10-28 11:07:07 +0200

    a

--- Wot.py
+++ Wot.py
@@ -68,7 +68,7 @@ ALREADY_TAKEN = "cant login, another player use this account"
 LOGIN_FAILED = "Login failed"
 
 # network
-IP = "192.168.1.20"
+IP = "192.168.43.252"
 SERVER_PORT = 2020
 GAME_PORT = 5120
 STREAM_OUTPUT_PORT = 32000
