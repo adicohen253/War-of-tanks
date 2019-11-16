3ada3ce7e486feaa9599805fef66c36b962000a9
Adi Cohen <55681305+adicohen253@users.noreply.github.com>
2019-11-16 23:43:16 +0200

    latest

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,7 @@
-function MyName()
-{
-  window.alert(document.getElementById("demo").value)
+function MyName() {
+    window.alert(document.getElementById("demo").value)
+}
+
+function MyFunc() {
+    window.location.href = 'https://www.google.com';
 }
