12926704601c18fc57b7d398d0be358eb63f8d09
adicohen253 <55681305+adicohen253@users.noreply.github.com>
2019-12-13 16:32:41 +0200

    latest

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import socket
 import pyaudio
 
-
+IP = "192.168.1.20"
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
@@ -10,6 +10,7 @@ WIDTH = 2
 STREAM_OUTPUT_PORT = 32000
 
 def voice_stream_connector(enemy_ip):
+    print("connect")
     stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         stream_socket.connect((enemy_ip, STREAM_OUTPUT_PORT))
@@ -17,17 +18,18 @@ def voice_stream_connector(enemy_ip):
         return
     p = pyaudio.PyAudio()
     try:
-        stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,
-                        output=True, frames_per_buffer=CHUNK)
+        microphone = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+        speaker = p.open(format=p.get_format_from_width(WIDTH),
+                         channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
         while True:
             try:
-                data = stream.read(CHUNK)
+                data = microphone.read(CHUNK)
                 stream_socket.send(data)
-                stream.write(stream_socket.recv(CHUNK))
+                speaker.write(stream_socket.recv(CHUNK))
             except (IOError, socket.error):
                 break
-        stream.stop_stream()
-        stream.close()
+        microphone.stop_stream()
+        microphone.close()
         p.terminate()
     except OSError:
         pass
@@ -35,6 +37,7 @@ def voice_stream_connector(enemy_ip):
 
 
 def voice_stream_creator(ip):
+    print("create")
     stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         stream_socket.bind((ip, STREAM_OUTPUT_PORT))
@@ -44,18 +47,20 @@ def voice_stream_creator(ip):
         return
     p = pyaudio.PyAudio()
     try:
-        stream = p.open(format=p.get_format_from_width(WIDTH), channels=CHANNELS,
-                        rate=RATE, output=True, input=True, frames_per_buffer=CHUNK)
+        microphone = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+        speaker = p.open(format=p.get_format_from_width(WIDTH),
+                         channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+
         while True:
             try:
-                data = stream.read(CHUNK)
+                data = microphone.read(CHUNK)
                 client.send(data)
-                stream.write(client.recv(CHUNK))
+                speaker.write(client.recv(CHUNK))
             except (IOError, socket.error):
                 break
 
-        stream.stop_stream()
-        stream.close()
+        microphone.stop_stream()
+        microphone.close()
         p.terminate()
         client.close()
     except OSError:
@@ -64,9 +69,8 @@ def voice_stream_creator(ip):
 
 
 def main():
-    ip = input("enter ip address: ")
-    print("start")
-    voice_stream_creator(ip)
+    # voice_stream_creator(IP)
+    voice_stream_connector(IP)
 
 
 if __name__ == '__main__':
